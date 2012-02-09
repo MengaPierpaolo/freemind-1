25d12238533eeb78014dbee775ea59f18b1e2c14
Ethan Mick <ethanmski@gmail.com>
2012-02-08 21:44:15 -0500

    Added another test on HistoryInformration

---
+++ freemind/tests/freemind/HistoryInformationTest.java
@@ -0,0 +1,29 @@
+package tests.freemind;
+
+import java.util.Date;
+
+import freemind.modes.HistoryInformation;
+
+public class HistoryInformationTest extends FreeMindTestBase {
+	
+	private HistoryInformation info;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		info = new HistoryInformation();
+	}
+	
+	public void testGetCreatedAt() {
+		Date now = new Date();
+		info = new HistoryInformation(now, now);
+		assertEquals(now, info.getCreatedAt());
+	}
+	
+	public void testGetLastModifiedAt() {
+		Date now = new Date();
+		info = new HistoryInformation(now, now);
+		assertEquals(now, info.getLastModifiedAt());
+	}
+
+}
